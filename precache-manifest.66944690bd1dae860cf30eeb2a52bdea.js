4c691ae510071e24db8304ce5a41b92165f5458f
Gabriel Donato <gdonato3@hotmail.com>
2019-06-20 16:43:31 -0300

    Add files via upload

---
+++ precache-manifest.66944690bd1dae860cf30eeb2a52bdea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "721c65cb00dc0bb63795",
+    "url": "/static/js/main.6059e26c.chunk.js"
+  },
+  {
+    "revision": "e9dd6b1c6252ef29259e",
+    "url": "/static/js/2.7036faba.chunk.js"
+  },
+  {
+    "revision": "e9dd6b1c6252ef29259e",
+    "url": "/static/css/2.c0254de3.chunk.css"
+  },
+  {
+    "revision": "adbd0f0f409770640f2d6d55da1c13c1",
+    "url": "/index.html"
+  }
+];
